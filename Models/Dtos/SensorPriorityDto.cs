0bbe3ff8ca467ca2361f4a00c96a96a7e4f1e82a
yuxuan <xuanyuong@gmail.com>
2024-03-08 17:57:14 +0800

    Changed the algorithm for displaying the sensors that are in priority.Displayed average data table and last week statistics graphs and improved css layout of the dashboard page. Started to work on the system details page.

---
+++ Models/Dtos/SensorPriorityDto.cs
@@ -0,0 +1,12 @@
+﻿namespace AquaCare_Web_App.Models.Dtos
+{
+    public class SensorPriorityDto
+    {
+        public int Id { get; set; }
+        public string Model { get; set; } = "";
+        public string Property { get; set; } = "";
+        public decimal PropertyValue { get; set; }
+        public decimal PriorityAbsolute { get; set; }
+        public decimal Priority { get; set; }
+    }
+}
